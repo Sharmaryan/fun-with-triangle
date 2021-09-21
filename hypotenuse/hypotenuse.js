fe20e9973beefc8178ffe7c6add7d0cf2b7174a2
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-09-21 22:04:45 +0530

    fix : added validation

--- hypotenuse/hypotenuse.js
+++ hypotenuse/hypotenuse.js
@@ -1,23 +1,35 @@
-const baseInput = document.querySelector('#base-value');
-const heigtInput = document.querySelector('#height-value');
-const calculateBtn = document.querySelector('#calculate-btn');
-const output = document.querySelector('#output');
+const baseInput = document.querySelector("#base-value");
+const heigtInput = document.querySelector("#height-value");
+const calculateBtn = document.querySelector("#calculate-btn");
+const output = document.querySelector("#output");
 
-function calculateHypotenuse(){
-    if(baseInput.value && heigtInput.value){
-
-        const baseInputValue = Number(baseInput.value);
-        const heightInputValue = Number(heigtInput.value);
-        const baseInputValueSquare = baseInputValue * baseInputValue;
-        const heightInputValueSquare = heightInputValue * heightInputValue;
-        
-        
-        const hypotenuseLength = Math.sqrt(baseInputValueSquare + heightInputValueSquare);
-        output.innerText = `The length of hypotenuse is ${hypotenuseLength}`;
-    }
-    else{
-        output.innerText = 'Please Enter Value in Both Fieds'
-    }
+function showMsg(msg) {
+  output.innerText = msg;
 }
 
-calculateBtn.addEventListener('click', calculateHypotenuse);
+function calculateHypotenuse() {
+  if (baseInput.value && heigtInput.value) {
+    if (baseInput.value <= 0 || heigtInput.value <= 0) {
+      const message = `Entered value should be greater than zero`;
+      showMsg(message);
+    } else {
+      const baseInputValue = Number(baseInput.value);
+      const heightInputValue = Number(heigtInput.value);
+      const baseInputValueSquare = baseInputValue * baseInputValue;
+      const heightInputValueSquare = heightInputValue * heightInputValue;
+
+      const hypotenuseLength = Math.sqrt(
+        baseInputValueSquare + heightInputValueSquare
+      );
+      const message = `The length of hypotenuse is ${hypotenuseLength.toFixed(
+        2
+      )}`;
+      showMsg(message);
+    }
+  } else {
+    const message = `Please Enter Value in Both Fieds`;
+    showMsg(message);
+  }
+}
+
+calculateBtn.addEventListener("click", calculateHypotenuse);
