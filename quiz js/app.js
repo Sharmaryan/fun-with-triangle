127f1825f2b927f6a9bd6f4a00b2965b5d301cb2
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-09-21 21:59:44 +0530

    added new answers

--- quiz js/app.js
+++ quiz js/app.js
@@ -1,6 +1,6 @@
-const form = document.querySelector('.quiz-form');
-const submitBtn = document.querySelector('.submit-btn');
-const showOutput = document.querySelector('.output');
+const form = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector(".submit-btn");
+const showOutput = document.querySelector(".output");
 
 const answerList = [
   "3",
@@ -9,18 +9,23 @@ const answerList = [
   "isosceles",
   "equilateral",
   "Equilateral triangle",
+  "Pythagoras",
+  "Polygon",
+  "2",
+  "False",
 ];
 
-function calculateScore(){
-    const formResults = new FormData(form);
-    let score = 0, index = 0;
-    for(let value of formResults.values()){
-        if(value === answerList[index]){
-            score = score + 1;
-        }
-        index = index + 1;
+function calculateScore() {
+  const formResults = new FormData(form);
+  let score = 0,
+    index = 0;
+  for (let value of formResults.values()) {
+    if (value === answerList[index]) {
+      score = score + 1;
     }
-    showOutput.innerText = `The Score is ${score}`;
+    index = index + 1;
+  }
+  showOutput.innerText = `The Score is ${score}`;
 }
 
-submitBtn.addEventListener('click', calculateScore);
+submitBtn.addEventListener("click", calculateScore);
